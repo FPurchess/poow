ea3b6e3f255d33009ef99153cd275c5ec32cf7e5
Florian Purchess <florian@purchess.de>
2014-03-22 17:26:49 +0100

    moved assets to public/images

--- app/scenes/loading.js
+++ app/scenes/loading.js
@@ -12,14 +12,14 @@ Crafty.scene('Loading', function () {
     Crafty.load([
 
         // sprites
-        'assets/player.png',
+        'public/images/player.png',
 
         // audio
         'public/music/ouroboros.mp3'
 
     ], function () {
-        Crafty.sprite('assets/player.png', {spr_player: [0, 0, 50, 50]});
-        Crafty.sprite('assets/bullet.jpg', {spr_bullet: [0, 0, 50, 50]});
+        Crafty.sprite('public/images/player.png', {spr_player: [0, 0, 50, 50]});
+        Crafty.sprite('public/images/bullet.jpg', {spr_bullet: [0, 0, 50, 50]});
 
         // start!
         Crafty.scene('Game');
